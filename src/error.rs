fdd14b43b3d4ea65de8dd8a396a042b6eca647e5
Pinku Deb Nath <prantoran@gmail.com>
2025-07-26 16:37:23 -0700

    Client error response using mapper

--- src/error.rs
+++ src/error.rs
@@ -3,7 +3,7 @@ use reqwest::StatusCode;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, strum_macros::AsRefStr)]
 #[allow(dead_code)]
 pub enum Error {
     LoginFail,
@@ -16,6 +16,7 @@ pub enum Error {
     // - Auth errors
     AuthFailNoAuthTokenCookie,
     AuthFailTokenWrongFormat,
+    AuthFailCtxNotInRequestExt
 }
 
 impl std::fmt::Display for Error {
@@ -30,6 +31,44 @@ impl IntoResponse for Error {
     fn into_response(self) -> Response<axum::body::Body> {
         println!("->> {:<12} - {self:?}", "INTO_RES");
         
-        (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+        // Create a placeholder Axum response
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+
+        // Insert the error into the response
+        response.extensions_mut().insert(self);
+
+        response
     }
+}
+
+impl Error {
+    pub fn clien_status_and_error(&self) -> (StatusCode, ClientError) {
+        match self {
+            Self::LoginFail => (StatusCode::FORBIDDEN, ClientError::LOGIN_FAIL),
+            // - Auth errors
+            Self::AuthFailNoAuthTokenCookie
+                | Self::AuthFailTokenWrongFormat
+                | Self::AuthFailCtxNotInRequestExt => {
+                (StatusCode::FORBIDDEN, ClientError::NO_AUTH)
+            }
+            // - Model errors
+            Self::TicketDeleteFailIdNotFound { id } => {
+                (StatusCode::BAD_REQUEST, ClientError::INVALID_PARAMS)
+            }
+            // - Fallback
+            _ => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                ClientError::SERVICE_ERROR,
+            ),
+        }
+    }
+}
+
+#[derive(Debug, strum_macros::AsRefStr)]
+#[allow(non_camel_case_types)]
+pub enum ClientError {
+    LOGIN_FAIL,
+    NO_AUTH,
+    INVALID_PARAMS,
+    SERVICE_ERROR,
 }
--- src/middlewares/mappers.rs
+++ src/middlewares/mappers.rs
@@ -1,11 +1,38 @@
 
 use axum::{
-    http::Response
+    http::Response, response::IntoResponse, Json
 };
+use serde_json::json;
+use uuid::Uuid;
+
+use crate::error::Error;
 
 pub async fn main_response_mapper(res: Response<axum::body::Body>) -> Response<axum::body::Body> {
     println!("->> {:<12} - main_response_mapper", "MIDDLEWARE");
+    let uuid = Uuid::new_v4();
 
+    // -- Get the eventual response error.
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.clien_status_and_error());
+    
+    // --If client error, build the new response.
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                "error": {
+                    "type": client_error.as_ref(),
+                    "req_uuid": uuid.to_string(),
+                }
+            });
+
+            println!("->> {:<12} - {client_error_body}", "CLIENT_ERROR");
+
+            (*status_code, Json(client_error_body)).into_response()
+        });
+    
+    // -- TODO: Build and log the server log line.
+    println!("   ->> server log line - {uuid} - Error: {service_error:?}");
     println!();
-    res
+    error_response.unwrap_or(res)
 }
